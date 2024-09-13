29a1f6d7f5261587d0bf6c5bfd8056a94679b19a
CaioDetore <caio.detore@sou.unifeob.edu.br>
2024-09-13 19:21:52 -0300

    feat: componentes ui + interface criar e visualizar metas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,13 @@
-import logo from "./assets/logo.svg";
-import letStart from "./assets/let-start-illustration.svg";
-import { Plus } from "lucide-react";
+import { Dialog } from "./components/ui/dialog";
+import { CreateGoal } from "./components/create-goal";
+import { Summary } from "./components/summary";
 
 export function App() {
   return (
-    <div className="h-scren flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="logo in.orbit" />
-      <img src={letStart} alt="logo in.orbit" />
-      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
-      </p>
-
-      <button
-        type="button"
-        className="px-4 py-2.5 flex rounded-lg items-center bg-violet-500 text-violet-50 gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
-      >
-        <Plus className="size-4" />
-        Cadastrar meta
-      </button>
-    </div>
+    <Dialog>
+      {/* <EmptyGoals /> */}
+      <Summary />
+      <CreateGoal />
+    </Dialog>
   );
 }
